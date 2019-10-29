962777ba8d274411ad1ec511bffbfa5c4a6bab58
Bouwe Ceunen <bouwe.ceunen@rombit.be>
2019-10-29 20:32:31 +0100

    swap

--- dockers/vision/vision/start.sh
+++ dockers/vision/vision/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # create swap
-mkdir -p $(dirname $SWAPFILE)
-dd if=/dev/zero of=$SWAPFILE bs=1024k count=$SWAPSIZE
-mkswap $SWAPFILE
-chmod 0600 $SWAPFILE
-swapon $SWAPFILE
-echo "$SWAPFILE none swap sw 0 0" >> /etc/fstab
-echo "created swap file"
+# mkdir -p $(dirname $SWAPFILE)
+# dd if=/dev/zero of=$SWAPFILE bs=1024k count=$SWAPSIZE
+# mkswap $SWAPFILE
+# chmod 0600 $SWAPFILE
+# swapon $SWAPFILE
+# echo "$SWAPFILE none swap sw 0 0" >> /etc/fstab
+# echo "created swap file"
 
 # start inference
 python3 -u vision.py
