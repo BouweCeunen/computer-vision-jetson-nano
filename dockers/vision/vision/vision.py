25da93be8f4fa87f42034cc13928377f324ffd18
Bouwe Ceunen <bouwe.ceunen@rombit.be>
2019-10-30 22:07:02 +0100

    logging

--- dockers/vision/vision/vision.py
+++ dockers/vision/vision/vision.py
@@ -10,6 +10,18 @@ import threading
 import time
 import cv2
 
+def tracefunc(frame, event, arg, indent=[0]):
+      if event == "call":
+          indent[0] += 2
+          print("-" * indent[0] + "> call function", frame.f_code.co_name)
+      elif event == "return":
+          print("<" + "-" * indent[0], "exit function", frame.f_code.co_name)
+          indent[0] -= 2
+      return tracefunc
+
+import sys
+sys.settrace(tracefunc)
+
 try:
     IMAGE_OVERLAY = str(os.environ['IMAGE_OVERLAY'])
 except KeyError:
